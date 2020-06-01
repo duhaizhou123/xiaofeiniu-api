e3b1230f3e3b5a10ae781938b4ab08887788e7d2
unknown <942216863@qq.com>
2020-06-01 23:35:38 +0800

    category相关路由开发

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 const PORT = 8090;
 const express = require('express');
+const cors = require('cors');
 const categoryRouter = require('./routes/admin/category');
 
 //创建HTTP应用服务器
@@ -11,6 +12,9 @@ var app = express();
 app.listen(PORT,()=>{
 	console.log('Server Listening: '+PORT );
 });
+
+//使用中间件
+app.use(cors());
 
 //挂载路由器
 app.use('/admin/category',categoryRouter);
--- routes/admin/category.js
+++ routes/admin/category.js
@@ -3,7 +3,7 @@
 */
 //创建路由器
 const express = require('express');
-const pool = require('./pool');
+const pool = require('../../pool');
 var router = express.Router();
 module.exports = router;
 
@@ -13,14 +13,14 @@ module.exports = router;
 *返回值形如：
 *[{cid: 1, cname: '..'},{}]
 */
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
 	pool.query('SELECT * FROM xfn_category ORDER BY cid',
-	(err,result)=>{
-		if(err) throw err;
-		res.send(result);
-	}
-	)
+		(err, result) => {
+			if (err) throw err;
+			res.send(result);
+		})
 })
+
 /*
 *API: DELETE /admin/category/:cid
 *含义：根据表示菜品编号的路由参数，删除该菜品
@@ -28,7 +28,23 @@ router.get('/',(req,res)=>{
 *{code: 200, msg: '1 category delete'}
 *{code: 400, msg: '0 category delete'}
 */
+router.delete('/:cid', (req, res) => {
+	//删除菜品类别前必须先把属于该类别菜品的类别编号设置为NULL
+	pool.query('UPDATE xfn_dish SET categoryId=NULL WHERE categoryId=?', req.params.cid, (err, result) => {
+		if (err) throw err;
+		pool.query('DELETE FROM xfn_category WHERE cid=?',
+			req.params.cid, (err, result) => {
+				if (err) throw err;
+				//获取DELETE语句在数据库中的影响行数
+				if (result.affectedRows > 0) {
+					res.send({ code: 200, msg: '1 category delete' });
+				} else {
+					res.send({ code: 400, msg: '0 category delete' });
+				}
+			})
+	})
 
+})
 /*
 *API: POST /admin/category
 *请求参数：{cname: 'xxx'}
