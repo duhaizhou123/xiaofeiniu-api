73e7fca8d7ddd7c40bdd2a517389d9920f4eb690
unknown <942216863@qq.com>
2020-06-09 23:40:36 +0800

    table相关路由补充开发完成

--- routes/admin/table.js
+++ routes/admin/table.js
@@ -55,3 +55,73 @@ router.get('/inuse/:tid',(req,res)=>{
 	})
 })
 
+/**
+ * API: PATCH /admin/table
+ * 含义：修改桌台信息
+ * 响应数据：
+ * {code: 200, msg: 'modified table detail success'}
+ * {code: 400,msg: 'modified table detail success, no modification'}
+ */
+router.patch('/',(req,res)=>{
+	var data = req.body;
+	pool.query('UPDATE xfn_table SET ? WHERE tid=?',[data,data.tid],(err,result)=>{
+		if(err) throw err;
+		if(result.changedRows == 0){
+			res.send({code: 400,msg: 'modified table status success, no modification'});
+		}else if(result.changedRows>0){
+			res.send({code: 200, msg: 'modified table status success'});
+		}
+	})
+})
+
+/**
+ * API: POST /admin/table
+ * 含义：添加桌台
+ * 请求参数：
+ * {tname: 'xxx', type: 'xxx', status: x}
+ * 响应参数：
+ * {code: 200, msg: 'added table success, tid: xx'}
+ * {code: 400,msg: 'added table fail, table name is already exists'}
+ */
+router.post('/',(req,res)=>{
+	var data = req.body;
+	pool.query('SELECT tid FROM xfn_table WHERE tname=?',data.tname,(err,result)=>{
+		if(err) throw err;
+		if(result.length>0){//如果桌名已经存在不能添加
+			res.send({code: 400, msg: 'added table fail, table name is already exists'});
+			return;
+		}
+	})
+	//如果桌名不存在可以添加桌台
+	pool.query('INSERT INTO xfn_table SET ?',data,(err,result)=>{
+		if(err) throw err;
+		res.send({code: 200, msg: 'added table success', tid: result.insertId});
+	})
+})
+
+/**
+ * API: DELETE /admin/table/:tid
+ * 含义：删除桌台
+ * 响应参数：
+ * {code: 200, msg: '1 table delete'}
+ * {code: 400, msg: '0 table delete'}
+ */
+router.delete('/:tid',(req,res)=>{
+	pool.query('UPDATE xfn_order SET tableId=NULL WHERE tableId=?',req.params.tid,(err,result)=>{
+		if(err) throw err;
+		pool.query('UPDATE xfn_reservation SET tableId=NULL WHERE tableId=?',req.params.tid,(err,resutl)=>{
+			if(err) throw err;
+			pool.query('DELETE FROM xfn_table WHERE tid=?',req.params.tid,(err,result)=>{console.log(result);
+				if(err) throw err;
+				if(result.affectedRows > 0){
+					res.send({code: 200, msg: '1 table delete'});
+				}else if(result.affectedRows == 0){
+					res.send({code: 400, msg: '0 table delete'});
+				}
+				
+			})
+		})
+		
+	})	
+})
+
